f3d75a011d66974f71b6615789c5620a533f63d0
Manu @StuFFmc Carrasco Molina <m+git@carrascomolina.com>
2025-01-10 13:29:30 +0100

    For now, using a Toggle to know which App to launch in the Simulator
    I need to research later how to know which app opened me

--- Connect/App.swift
+++ Connect/App.swift
@@ -3,14 +3,23 @@ import SwiftUI
 @main
 struct Garmin_Connect_Simulator: App {
     @State var text = "Garmin Connect Simulator"
+    @AppStorage("swiftUI") var swiftUI = true
     @Environment(\.openURL) private var openURL
 
     var body: some Scene {
         WindowGroup {
-            Text(text)
+            Toggle("SwiftUI", isOn: $swiftUI)
+                .font(.largeTitle)
+                .padding(100)
                 .onOpenURL {
                     text = $0.absoluteString
-                    openURL(URL(string: "garmin-exampleapp-swift://device-select-resp?ciqApp=Connect&ciqBundle=com.garmin.connect.mobile&ciqScheme&ciqSdkVersion=10000&sourceUrlHost&d0ID=A6230408-B526-A5D6-8929-7791342EB4AF&d0Model=fenix%205&d0Name=garsim01")!)
+                    Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+                        openURL(
+                            URL(
+                                string: "garmin-exampleapp-swift\(swiftUI ? "ui" : "")://device-select-resp?ciqApp=Connect&ciqBundle=com.garmin.connect.mobile&ciqScheme&ciqSdkVersion=10000&sourceUrlHost&d0ID=A6230408-B526-A5D6-8929-7791342EB4AF&d0Model=fenix%205&d0Name=garsim01"
+                            )!
+                        )
+                    }
                 }
         }
     }

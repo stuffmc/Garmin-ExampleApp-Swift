6aa3f7ccd0bc3ed39ca28fd0539dae71e1518020
Manu @StuFFmc Carrasco Molina <m+git@carrascomolina.com>
2025-01-10 13:19:44 +0100

    Adding a mock of a reply with one Garmin Device

--- Connect/App.swift
+++ Connect/App.swift
@@ -10,7 +10,7 @@ struct Garmin_Connect_Simulator: App {
             Text(text)
                 .onOpenURL {
                     text = $0.absoluteString
-                    openURL(URL(string: "garmin-exampleapp-swiftui://")!)
+                    openURL(URL(string: "garmin-exampleapp-swift://device-select-resp?ciqApp=Connect&ciqBundle=com.garmin.connect.mobile&ciqScheme&ciqSdkVersion=10000&sourceUrlHost&d0ID=A6230408-B526-A5D6-8929-7791342EB4AF&d0Model=fenix%205&d0Name=garsim01")!)
                 }
         }
     }
